dabfc099f49b2ca6fa2601682d6398f531ba4aad
jamiecartin <jamie.cartin@icloud.com>
2023-03-24 09:39:38 -0400

    Initial Commit

---
+++ MeditateTime/MeditateTimeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MeditateTimeApp.swift
+//  MeditateTime
+//
+//  Created by Jamie Cartin on 3/24/23.
+//
+
+import SwiftUI
+
+@main
+struct MeditateTimeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
