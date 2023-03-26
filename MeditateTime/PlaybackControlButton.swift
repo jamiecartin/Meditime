31b44a342ab60dbd4a3514736427f16362fd4bb3
jamiecartin <jamie.cartin@icloud.com>
2023-03-26 13:10:33 -0400

    Setting playback view and controls

---
+++ MeditateTime/PlaybackControlButton.swift
@@ -0,0 +1,20 @@
+//
+//  PlaybackControlButton.swift
+//  MeditateTime
+//
+//  Created by Jamie Cartin on 3/26/23.
+//
+
+import SwiftUI
+
+struct PlaybackControlButton: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct PlaybackControlButton_Previews: PreviewProvider {
+    static var previews: some View {
+        PlaybackControlButton()
+    }
+}
--- MeditateTime/PlayerView.swift
+++ MeditateTime/PlayerView.swift
@@ -8,13 +8,56 @@
 import SwiftUI
 
 struct PlayerView: View {
+    @State private var value: Double = 0.0
+    
     var body: some View {
         ZStack {
+            
+            //MARK: Background Image
             Image("meditate")
                 .resizable()
                 .scaledToFill()
                 .frame(width: UIScreen.main.bounds.width)
                 .ignoresSafeArea()
+            
+            VStack(spacing: 32) {
+                
+                //MARK: Dismiss Button
+                HStack {
+                    Button {
+                        
+                    } label: {
+                        Image(systemName: "xmark.circle.fill")
+                            .font(.system(size: 36))
+                            .foregroundColor(.white)
+                    }
+                    Spacer()
+                }
+                
+                //MARK: Title
+                Text("1 Minute Relaxing Meditation")
+                    .font(.title)
+                    .foregroundColor(.white)
+                
+                Spacer()
+                
+                //MARK: Playback
+                VStack(spacing: 5) {
+                    Slider(value: $value, in: 0...60)
+                        .accentColor(.white)
+                    
+                    HStack {
+                        Text("0:00")
+                        
+                        Spacer()
+                        
+                        Text("1:00")
+                    }
+                    .font(.caption)
+                    .foregroundColor(.white)
+                }
+            }
+            .padding(20)
         }
     }
 }
